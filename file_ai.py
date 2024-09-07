64964420875e0220972385162a6a73e40e0d75b1
wenxijuji <wchen@juji-inc.com>
2024-09-07 15:18:26 -0700

    starting file

---
+++ file_ai.py
@@ -0,0 +1,29 @@
+import os
+
+# find all files and directories and their basic information in the current Windows directory
+def list_files():
+    """Returns a list of all files and directories in the current Windows directory along with their basic information."""
+    if not os.path.exists('.'):
+        raise FileNotFoundError('The current directory does not exist.')
+    if not os.path.isdir('.'):
+        raise NotADirectoryError('The current path is not a directory.')
+    try:
+        file_list = []
+        for file_or_dir in os.listdir():
+            file_path = os.path.join('.', file_or_dir)
+            file_list.append({
+                'name': file_or_dir,
+                'path': file_path,
+                'is_file': os.path.isfile(file_path),
+                'is_dir': os.path.isdir(file_path),
+                'size': os.path.getsize(file_path),
+                'modified': datetime.datetime.fromtimestamp(os.path.getmtime(file_path)),
+                'created': datetime.datetime.fromtimestamp(os.path.getctime(file_path)),
+                'accessed': datetime.datetime.fromtimestamp(os.path.getatime(file_path))
+            })
+        return file_list
+    except PermissionError:
+        raise PermissionError('Permission denied to access the current directory.')
+
+if __name__ == "__main__":
+    print(list_files())
